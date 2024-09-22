4f31a55efa0cb4fc1218eaf47b9fca3cc13d904e
Maja Szymajda <57510213+majaszymajda@users.noreply.github.com>
2024-09-22 14:57:24 +0200

    MAJI-80 feat(logging): add logging for email success (#3)
    * feat(logging): add logging for email success
    
    * feat(logging): add logging for email failure
    
    ---------
    
    Co-authored-by: Kamil Marut <kamil@kamilmarut.com>

--- django_nitro_mailer/tasks.py
+++ django_nitro_mailer/tasks.py
@@ -1,11 +1,15 @@
 import logging
-import pickle
-from django.db import transaction, models
-from django.core.mail import get_connection, send_mass_mail
+import os
 from typing import Optional
+
+import timezone
+from django.core.mail import get_connection
+from django.db import models, transaction
+
 from django_nitro_mailer.models import Email, EmailLog
 
 logger = logging.getLogger(__name__)
+email_db_logging = os.getenv("EMAIL_DB_LOGGING_ENABLED", "true").lower() == "true"
 
 
 def send_emails(queryset: Optional[models.QuerySet] = None) -> None:
@@ -21,11 +25,21 @@ def send_emails(queryset: Optional[models.QuerySet] = None) -> None:
                 if email_message:
                     connection.send_messages([email_message])
 
-                    EmailLog.objects.create(email_data=email_obj.email_data, result=EmailLog.Results.SUCCESS)
+                    if email_db_logging:
+                        EmailLog.objects.create(email_data=email_obj.email_data, result=EmailLog.Results.SUCCESS)
+
+                    logger.info(
+                        "Email sent successfully",
+                        extra={"recipients": email_obj.recipients, "created_at": timezone.now()},
+                    )
+
                     email_obj.delete()
+
                 else:
                     logger.error("Failed to retrieve email")
 
             except Exception as e:
-                EmailLog.objects.create(email_data=email_obj.email_data, result=EmailLog.Results.FAILURE)
+                if email_db_logging:
+                    EmailLog.objects.create(email_data=email_obj.email_data, result=EmailLog.Results.FAILURE)
+
                 logger.error("Failed to send email", exc_info=e)
